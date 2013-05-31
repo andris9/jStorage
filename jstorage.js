24421af78a2349f06f5bcf0296bf1862e1ef69f3
Pablo Lozano Olvera <pabloloz@gmail.com>
2013-05-31 08:24:43 -0500

    Fix running from localhost no workin in Global Storage Firefox 2
    Firefox 2 has an issue that non dotted domains will no be accepted.
    Using 'localhost.localdomain' as the host name is the recommended
    solution.

--- jstorage.js
+++ jstorage.js
@@ -176,6 +176,13 @@
         else if("globalStorage" in window){
             try {
                 if(window.globalStorage) {
+					if(window.location.hostname == 'localhost'){
+						var hostloc = 'localhost.localdomain';
+						_storage_service = window.globalStorage[hostloc];
+					}
+					else{
+						_storage_service = window.globalStorage[window.location.hostname];
+					}
                     _storage_service = window.globalStorage[window.location.hostname];
                     _backend = "globalStorage";
                     _observer_update = _storage_service.jStorage_update;
